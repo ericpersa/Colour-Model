f82ebda30382b2e1162fff1112f5fba6154b5487
Eric <ericpersabusiness@gmail.com>
2024-11-13 14:50:53 -0700

    Function prototype in pigment.h has been completed

---
+++ src/pigment.c
@@ -0,0 +1,15 @@
+#include"pigment.h"
+
+
+int loadPigmentData(char* filename, pigment_t* pArray, int* n) {
+    //purpose: 
+    //  loadPigmentData will read data line by line from the filename data location into a pigment_t  
+    //  structure
+    //input:
+    //  filename:
+    //      a pointer to the file where the data is stored
+    //  pArray:
+    //      an array of pigment structures where the data is stored
+    //  n:
+    //      a pointer to the number of entries found.
+}
