bdf96133a8943371eb4d4a1bb5067b640804d9b6
Eric <ericpersabusiness@gmail.com>
2024-11-13 14:54:00 -0700

    Removed unncecessary function implementation in header file

--- src/pigment.h
+++ src/pigment.h
@@ -1,14 +1,2 @@
 // function prototype for question 4. function will be used to load data into an array of pigment structures
 int loadPigmentData(char* filename, pigment_t* pArray, int* n);
-
-int loadPigmentData(char* filename, pigment_t* pArray, int* n) {
-    //purpose: loadPigmentData will read data line by line from data/data.tar into a pigment_t
-    //                                                                              structure
-    //input:
-    //  filename:
-    //      a pointer to the file where the data is stored
-    //  pArray:
-    //      an array of pigment structures where the data is stored
-    //  n:
-    //      a pointer to the number of entries found.
-}
