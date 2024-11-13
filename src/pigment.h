a8769c3aa7367ae633ca5c7ec97ec399368570ef
Eric <ericpersabusiness@gmail.com>
2024-11-13 15:06:38 -0700

    Created the structure declaration called pigmentData which will be used to store the data of the pigments. pigmentData has been made in pigment.h so it can be used across multiple files.

--- src/pigment.h
+++ src/pigment.h
@@ -1,2 +1,13 @@
 // function prototype for question 4. function will be used to load data into an array of pigment structures
 int loadPigmentData(char* filename, pigment_t* pArray, int* n);
+
+struct pigmentData {
+    float abHp[2];
+    int hueAngle;
+    int chroma;
+    float huePurity;
+    int abValue[2];
+    int value;
+    char* ciName;
+    char* pigmentName;
+};
