5eff9f10806338fed07da4ed37fa6a92334ae957
Eric <ericpersabusiness@gmail.com>
2024-11-13 15:27:59 -0700

    Testing if filepath works. Read from file is succesful. Files need to be cleaned up as I left a bit of a mess.

--- src/pigment.c
+++ src/pigment.c
@@ -1,15 +1,34 @@
+#include<stdio.h>
 #include"pigment.h"
 
+int main(void) {
+    // lets just try to parse through the lines in data/data.dat
+    FILE* fp = fopen("../data/data.tar", "r");
 
-int loadPigmentData(char* filename, pigment_t* pArray, int* n) {
-    //purpose: 
-    //  loadPigmentData will read data line by line from the filename data location into a pigment_t  
-    //  structure
-    //input:
-    //  filename:
-    //      a pointer to the file where the data is stored
-    //  pArray:
-    //      an array of pigment structures where the data is stored
-    //  n:
-    //      a pointer to the number of entries found.
+    if (fp == NULL) {
+        printf("Error opening file");
+        return 1;
+    } 
+    char line[256]; // buffer for each line
+    while (fgets(line, sizeof(line), fp)) {
+        // Each line is now stored in 'line' as a string, up to 255 characters
+        printf("%s", line);  // Print or process the line as needed
+    }
+
+    fclose(fp);  // Close the file when done
+    return 0;
 }
+
+
+// int loadPigmentData(char* filename, pigment_t* pArray, int* n) {
+//     //purpose: 
+//     //  loadPigmentData will read data line by line from the filename data location into a pigment_t  
+//     //  structure
+//     //input:
+//     //  filename:
+//     //      a pointer to the file where the data is stored
+//     //  pArray:
+//     //      an array of pigment structures where the data is stored
+//     //  n:
+//     //      a pointer to the number of entries found.
+// }
--- src/pigment.h
+++ src/pigment.h
@@ -1,7 +1,7 @@
 // function prototype for question 4. function will be used to load data into an array of pigment structures
-int loadPigmentData(char* filename, pigment_t* pArray, int* n);
+// int loadPigmentData(char* filename, pigment_t* pArray, int* n);
 
-struct pigmentData {
+struct pigment_t {
     float abHp[2];
     int hueAngle;
     int chroma;
