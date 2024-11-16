d0fec4b2965ad752f6bfa741ecf5cf905c4361ab
jennaleew <122505663+jennaleew@users.noreply.github.com>
2024-11-16 00:08:27 +0000

    uncomment

--- src/pigment.h
+++ src/pigment.h
@@ -1,16 +1,16 @@
 // function prototype for question 4. function will be used to load data into an array of pigment structures
 // int loadPigmentData(char* filename, pigment_t* pArray, int* n);
 
-// struct pigment_t {
-//     float abHp[2];
-//     int hueAngle;
-//     int chroma;
-//     float huePurity;
-//     int abValue[2];
-//     int value;
-//     char* ciName;
-//     char* pigmentName;
-// }
+struct pigment_t {
+    float abHp[2];
+    int hueAngle;
+    int chroma;
+    float huePurity;
+    int abValue[2];
+    int value;
+    char* ciName;
+    char* pigmentName;
+}
 
 typedef struct paint_t{
     char ciName[30];
