1d14ab8190dd2a5cb36220d040fe606cea4ce4ea
jennaleew <122505663+jennaleew@users.noreply.github.com>
2024-11-20 20:11:19 +0000

    reformat paint test printing

--- src/test_load.c
+++ src/test_load.c
@@ -25,45 +25,54 @@ int main(void){
         exit(EXIT_FAILURE);
     }
 
-    int testnum = 5;
+    int testnum = 0;
     // print 
     printf("Success. Loaded paints:\n");
-    printf("testing line %i in array:\n", testnum);
-    printf("ci:%s\n Mn:%s\n man:%s\n", paint_arr[testnum].ciName, paint_arr[testnum].marketingName, paint_arr[testnum].manufacturer);
-    printf("code:%i\n tran:%i\n sta:%i\n VR:%i\n", paint_arr[testnum].code, paint_arr[testnum].transparency, paint_arr[testnum].staining, paint_arr[testnum].valueRange);
-    printf("gr:%i\n bl:%i\n di:%i\n", paint_arr[testnum].granulating, paint_arr[testnum].blossom, paint_arr[testnum].diffusion);
-    printf("HA:%i\n HS:%i\n LF:%i\n", paint_arr[testnum].hueAngle, paint_arr[testnum].hueShift, paint_arr[testnum].lightfast);
+    printf("Paint %i\n:", testnum);
+    printf("CI Name: %s\n", paint_arr[testnum].ciName);
+    printf("Marketing name: %s\n", paint_arr[testnum].marketingName);
+    printf("Manufacturer: %s\n", paint_arr[testnum].manufacturer);
+    printf("code: %i\n", paint_arr[testnum].code);
+    printf("transparency: %i\n", paint_arr[testnum].transparency);
+    printf("staining: %i\n", paint_arr[testnum].staining);
+    printf("Value range: %i\n", paint_arr[testnum].valueRange);
+    printf("granulating: %i\n", paint_arr[testnum].granulating);
+    printf("blossom: %i\n", paint_arr[testnum].blossom);
+    printf("diffusion: %i\n", paint_arr[testnum].diffusion);
+    printf("Hue angle: %i\n", paint_arr[testnum].hueAngle);
+    printf("Hue shift: %i\n", paint_arr[testnum].hueShift);
+    printf("Lightfast: %i\n", paint_arr[testnum].lightfast);
 
 
     // erics tests
-    char* filename = "../data/pigments.dat";
-    pigment_t* pigments = NULL; // Use pigments to store the returned array
-    int numPigments = 0;        // Number of pigments loaded
+    // char* filename = "../data/pigments.dat";
+    // pigment_t* pigments = NULL; // Use pigments to store the returned array
+    // int numPigments = 0;        // Number of pigments loaded
 
     // Call loadPigmentData
-    pigments = loadPigmentData(filename, NULL, &numPigments);
+    // pigments = loadPigmentData(filename, NULL, &numPigments);
 
-    if (pigments == NULL) {
-        printf("Error: Failed to load pigment data.\n");
-        return 1;
-    }
+    // if (pigments == NULL) {
+    //     printf("Error: Failed to load pigment data.\n");
+    //     return 1;
+    // }
 
     // test pigments
-    printf("Success. Loaded %d pigments:\n", numPigments);
-    for (int i = 0; i < numPigments; i++) {
-        printf("Pigment %d:\n", i+1);
-        printf("    CI Name: %s\n", pigments[i].ciName);
-        printf("    Pigment Name: %s\n", pigments[i].pigmentName);
-        printf("    Value: %d\n", pigments[i].value);
-        printf("    Chroma: %d\n", pigments[i].chroma);
-        printf("    Hue Angle: %d\n", pigments[i].hueAngle);
-        printf("    Hue Purity: %.2f\n", pigments[i].huePurity);
-        printf("    abValue: [%d, %d]\n", pigments[i].abValue[0], pigments[i].abValue[1]);
-        printf("    abHp: [%.2f, %.2f]\n", pigments[i].abHp[0], pigments[i].abHp[1]);
-    }
+    // printf("Success. Loaded %d pigments:\n", numPigments);
+    // for (int i = 0; i < numPigments; i++) {
+    //     printf("Pigment %d:\n", i+1);
+    //     printf("    CI Name: %s\n", pigments[i].ciName);
+    //     printf("    Pigment Name: %s\n", pigments[i].pigmentName);
+    //     printf("    Value: %d\n", pigments[i].value);
+    //     printf("    Chroma: %d\n", pigments[i].chroma);
+    //     printf("    Hue Angle: %d\n", pigments[i].hueAngle);
+    //     printf("    Hue Purity: %.2f\n", pigments[i].huePurity);
+    //     printf("    abValue: [%d, %d]\n", pigments[i].abValue[0], pigments[i].abValue[1]);
+    //     printf("    abHp: [%.2f, %.2f]\n", pigments[i].abHp[0], pigments[i].abHp[1]);
+    // }
 
-    // free memory
-    free(pigments);
+    // // free memory
+    // free(pigments);
 
     exit(EXIT_SUCCESS);
 }
